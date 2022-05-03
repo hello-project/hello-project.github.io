b314ddcab3dbd6af821019e40a6376791d83f2d4
hello-project <58546256+hello-project@users.noreply.github.com>
2022-05-03 15:14:09 +1000

    Updated capture filename

--- scripts/main.js
+++ scripts/main.js
@@ -425,7 +425,7 @@ function capture() {
         .then(canvas => {
             const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
             const a = document.createElement('a')
-            a.setAttribute('download', 'my-image.png')
+            a.setAttribute('download', 'hello-ranking.png')
             a.setAttribute('href', image)
             a.click()
             canvas.remove()
