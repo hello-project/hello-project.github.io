38f482119ddb6fca95ff1a9abe3e33bb346807eb
hello-project <58546256+hello-project@users.noreply.github.com>
2022-05-03 14:47:00 +1000

    Added capture js

--- scripts/main.js
+++ scripts/main.js
@@ -412,3 +412,25 @@ setLang();
 // checks the URL for a ranking and uses it to populate ranking
 getRanking();
 setDate();
+
+//New test code to save screen captures
+function capture() {
+    const captureElement = document.querySelector('#ranking__pyramid')
+    html2canvas(captureElement)
+        .then(canvas => {
+            canvas.style.display = 'none'
+            document.body.appendChild(canvas)
+            return canvas
+        })
+        .then(canvas => {
+            const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+            const a = document.createElement('a')
+            a.setAttribute('download', 'my-image.png')
+            a.setAttribute('href', image)
+            a.click()
+            canvas.remove()
+        })
+}
+
+const btn = document.querySelector('#btn')
+btn.addEventListener('click', capture)
